f0c02af3993d3b677f15b9fc740b1e02d8197126
Niko Kemppi <niko.kemppi@morssoftware.com>
2023-01-16 17:01:09 +0200

    Part1 unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -25,6 +25,10 @@ const App = () => {
     setBad(newValue)
   }
 
+  const allCount = good + neutral + bad
+  const average =  allCount > 0 ? (good - bad) / allCount : 0
+  const positive = allCount > 0 ? 100 * good / allCount : 0
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -35,6 +39,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {allCount}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
