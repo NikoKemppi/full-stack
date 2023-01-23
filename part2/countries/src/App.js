477481599775c95f0af3a81ab7c5e19722c13c77
Niko Kemppi <niko.kemppi@morssoftware.com>
2023-01-24 01:25:40 +0200

    Part2 countries step3

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,17 +1,71 @@
 import { useState, useEffect } from 'react'
 import axios from "axios";
 
-const getAll = () => {
+const api_key = process.env.REACT_APP_API_KEY
+//console.log("API key:", api_key)
+
+const getCountries = () => {
   const request = axios.get('https://restcountries.com/v3.1/all')
+  return request.then(response => response.data).catch(error => {console.log('fail')})
+}
+
+const getCapitalCoordinates = country => {
+  const request = axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${country.capital},${country.cca2}&limit=1&appid=${api_key}`)
+  return request.then(response => response.data).catch(error => {console.log('fail')})
+}
+
+const getCapitalWeather = (coords) => {
+  const lat = coords[0].lat
+  const lon = coords[0].lon
+  const request = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${api_key}`)
   return request.then(response => response.data).catch(error => {console.log('fail')})
 }
 
 const Finder = ({newFinder, handleFinderChange}) => {
   return (
     <form>
-      filter shown with <input value={newFinder} onChange={handleFinderChange}/>
+      find countries <input value={newFinder} onChange={handleFinderChange}/>
     </form>
   )
+}
+
+const Weather = ({country}) => {
+  const [weatherData, setWeatherData] = useState({});
+
+  useEffect(() => {
+    console.log("starting useEffect")
+    getCapitalCoordinates(country).then(cData => {
+      console.log("cData", cData)
+      getCapitalWeather(cData).then(wData => {
+        console.log("wData", wData)
+        setWeatherData(wData)
+      })
+    })}, [country])
+  
+  console.log("weatherData:", JSON.stringify(weatherData))
+  if (JSON.stringify(weatherData) === '{}') {
+    console.log("now loading")
+    return (
+      <div>
+        <p>Loading weather data...</p>
+      </div>
+    )
+  } else {
+    console.log("weatherData", weatherData)
+    console.log("after useEffet:")
+    const x = weatherData
+    if (x !== undefined) {
+      console.log("x:",x)
+      return (
+        <div>
+          <h3>Weather in {country.capital}</h3>
+          <p>temperature {x.main.temp} Celcius</p>
+          <img src={`http://openweathermap.org/img/wn/${x.weather[0].icon}@2x.png`}/>
+          <p>wind {x.wind.speed} m/s</p>
+        </div>
+      )
+    }
+  }
 }
 
 const Country = ({country}) => {
@@ -25,6 +79,7 @@ const Country = ({country}) => {
         {Object.values(country.languages).map((language, i) => <li key={i}>{language}</li>)}
       </ul>
       <img src={country.flags.png}/>
+      <Weather country={country}/>
     </div>
   )
 }
@@ -62,7 +117,7 @@ const App = () => {
   const [newFinder, setNewFinder] = useState('')
 
   useEffect(() => {
-    getAll()
+    getCountries()
     .then(initialCountries => {
       setCountries(initialCountries)
     })
